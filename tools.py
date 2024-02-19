f561b149e22aa27b1719f48f3afbd3533157fb1e
Deniz Ceylan <deniz.ceylan@nan-tic.com>
2024-02-19 17:33:51 +0100

    Added extra paremets for different input formats task #164637

--- tools.py
+++ tools.py
@@ -66,7 +66,7 @@ def js_to_html(content_block, url=''):
             case 'quote':
                 html += '<blockquote cite=' + block['data']['caption'] + '>' + block['data']['text'] + '</blockquote>'
             case 'image':
-                html += '<img src="' + url_from_tryton_to_flask(block['data']['file']['url'], url)+ '" />'
+                html += '<img src="' + url_from_tryton_to_flask(block['data']['url'], url)+ '" />'
             case 'link':
                 html += '<a href=' + block['data']['url'] + ' ></a>'
         if not was_p:
@@ -76,6 +76,18 @@ def js_to_html(content_block, url=''):
     html += '</body></html>'
     return html
 
+def js_to_text(js):
+    text = ''
+    js_object = json.loads(js)
+    for block in js_object['blocks']:
+        if 'text' in block['data']:
+            text += block['data']['text'].replace('<br>' , '\\n')
+        if 'items' in block['data']:
+            for entry in block['data']['items']:
+                text += '>' + str(entry).replace('<br>' , '') + '\\n'
+        text += '\\n'
+    return str(text)
+
 def text_to_js(text):
     '''
     Converts text to a dictionary of datablocks to be used with editorJS using
@@ -83,159 +95,173 @@ def text_to_js(text):
     '''
     datablocks = []
     text_blocks = text.split('\n')
-    for block in text_blocks:
+    for block in text_blocks: 
         if not block:
             continue
         current_block = {'data' : {}}
         block = block.strip('\n')
-        block = block.lstrip('\\')
-        match block[0]:
-            case '#':
-                current_block['type'] = 'header'
-                current_block['data']['level'] = len(block.split(' ')[0])
-                current_block['data']['text'] = block.strip('#').strip(' ')
-                datablocks.append(current_block.copy())
-            case '-':
-                if len(block) > 1:
-                    if block[1] and block[2]:
-                        if block[1] == '-' and block[2] == '-':
+        block = block.replace('\\', '')
+        block = block.strip()
+        if len(block) > 0:
+            block = block.replace('[![]', '[!]')
+            match block[0]:
+                case '#':
+                    current_block['type'] = 'header'
+                    current_block['data']['level'] = len(block.split(' ')[0])
+                    current_block['data']['text'] = block.strip('#').strip(' ')
+                    datablocks.append(current_block.copy())
+                case '-':
+                    if len(block) > 2:
+                        if block[1] and block[2]:
+                            if block[1] == '-' and block[2] == '-':
+                                current_block['type'] = 'delimeter'
+                                current_block['data'] = {}
+                                datablocks.append(current_block.copy())
+                            else:
+                                current_block['type'] = 'list'
+                                current_block['data']['style'] = 'unordered'
+                                current_block['data']['items'] = [b.strip('-') for b in block.split('\n')]
+                                datablocks.append(current_block.copy())
+                case '1':
+                    if len(block) > 1:
+                        if block[1] == '.':
+                            current_block['type'] = 'list'
+                            current_block['data']['style'] = 'ordered'
+                            current_block['data']['items'] = [b[2:] for b in block.split('\n')]
+                            datablocks.append(current_block.copy())
+                case '`':
+                    current_block['type'] = 'code'
+                    current_block['data']['code'] = block.strip('`')
+                    datablocks.append(current_block.copy())
+                    '''
+                case '>':
+                    current_block['type'] = 'quote'
+                    current_block['data']['text'] = block[1:]
+                    current_block['data']['caption'] = ''
+                    current_block['data']['alignment'] = 'left'
+                    datablocks.append(current_block.copy())
+                    '''
+                case '>':
+                    if len(block) > 1:
+                        if block[0] and block[1]:
+                            current_block['type'] = 'list'
+                            current_block['data']['style'] = 'unordered'
+                            current_block['data']['items'] = [b.strip('>') for b in block.split('\n')]
+                            datablocks.append(current_block.copy())
+                case '!':
+                    current_block['type'] = 'image'
+                    block = re.sub("!\[[^\]]*\]", '', block)
+                    current_block['data']['url'] = block.strip('[').split(':')[-1].strip(']').strip()
+                    #str(attachment_id_from_name(block.strip('[').split(':')[-1].strip(']')))
+                    datablocks.append(current_block.copy())
+                case '<':
+                    if re.match('\<image>', block):
+                        current_block['type'] = 'image'
+                        current_block['data']['url'] = block.strip('<').split(':')[-1].strip('>').strip()
+                        #str(attachment_id_from_name(block.strip('<').split(':')[-1].strip('>')))
+                        datablocks.append(current_block.copy())
+                case '[':
+                    if re.match('\[image', block) or re.match('\[!]', block):
+                        
+                        block = re.sub("\[!\]", '', block)
+                        current_block['type'] = 'image'
+                        current_block['data']['url'] = block.strip('(').strip(')')
+                        #str(attachment_id_from_name(block.strip('[').split(':')[-1].strip(']')))
+                        datablocks.append(current_block.copy())         
+                case _:
+                    current_block['type'] = 'paragraph'
+                    current_block['data']['text'] = block.strip(' ')
+                    if len(current_block['data']['text'].strip(' ')) > 0:
+                        datablocks.append(current_block.copy())
+    return json.dumps({'blocks': datablocks})
+
+def html_to_js(html_string, is_mail=False):
+    if not is_mail:
+        converted = html2text.html2text(html_string)
+        processed = text_to_js(converted)
+        return processed
+    else:
+        if html_string is '' or html_string is None:
+            return ''
+        datablocks = []
+        html_json = BeautifulSoup(html_string, 'lxml')
+        for component in html_json.find_all(recursive=False):
+            current_block = {'data': {}}
+            if not re.match(component.name, 'h[1-6]'):
+                if len(component) > 0:
+                    match component.name:
+                        case 'p':
+                            current_block['type'] = 'paragraph'
+                            current_block['data']['text'] = str(component.contents)
+                            datablocks.append(current_block.copy())
+                        case 'delimeter':
                             current_block['type'] = 'delimeter'
                             current_block['data'] = {}
                             datablocks.append(current_block.copy())
-                        else:
+                        case 'code':
+                            current_block['type'] = 'code'
+                            current_block['data']['code'] = str(component.contents).strip('`')
+                            datablocks.append(current_block.copy())
+                        case 'blockquote':
+                            current_block['type'] = 'quote'
+                            current_block['data']['caption'] = str(component.find('cite'))
+                            current_block['data']['text'] = str(component.find())
+                        case 'ul':
                             current_block['type'] = 'list'
                             current_block['data']['style'] = 'unordered'
-                            current_block['data']['items'] = [b.strip('-') for b in block.split('\n')]
+                            current_block['data']['items'] = []
+                            for list_entry in component.find_all('li'):
+                                current_block['data']['items'].append(list_entry.contents)
                             datablocks.append(current_block.copy())
-            case '1':
-                if block[1]:
-                    if block[1] == '.':
-                        current_block['type'] = 'list'
-                        current_block['data']['style'] = 'ordered'
-                        current_block['data']['items'] = [b[2:] for b in block.split('\n')]
-                        datablocks.append(current_block.copy())
-            case '`':
-                current_block['type'] = 'code'
-                current_block['data']['code'] = block.strip('`')
+                        case 'ol':
+                            current_block['type'] = 'list'
+                            current_block['data']['style'] = 'ordered'
+                            current_block['data']['items'] = []
+                            for list_entry in component.find_all('li'):
+                                current_block['data']['items'].append(list_entry.contents)
+                            datablocks.append(current_block.copy())
+                        case 'form':
+                            current_block['type'] = 'checklist'
+                            current_block['data']['items'] = []
+                            for input in component.find_all('input'):
+                                if input.find('checked') == 'true':
+                                    checked: 'True'
+                                else:
+                                    checked: 'False'
+                                checkbox = {
+                                    'checked': checked,
+                                    'text': str(input.find())
+                                }
+                                current_block['data']['items'].append(checkbox)
+                            datablocks.append(current_block.copy())
+                        case 'table':
+                            current_block['type'] = 'table'
+                            current_block['data'] = []
+                            current_block['data']['content'] = []
+                            for t_row in component.contents:
+                                if t_row.name == 'th':
+                                    current_block['data']['withHeadings'] = True
+                                if t_row.name == 'tr' or t_row.name == 'th':
+                                    row = []
+                                    for cell in t_row.contents:
+                                        if cell.name == 'td':
+                                            row.append(cell.contents[0])
+                                    current_block['data']['content'].append(row)
+                            datablocks.append(current_block.copy())
+                        case 'img':
+                            current_block['type'] = 'image'
+                            current_block['data']['file'] = {}
+                            current_block['data']['file']['url'] = str(component)
+                            datablocks.append(current_block.copy())
+                        case _:
+                            current_block['type'] = 'paragraph'
+                            current_block['data']['text'] = str(component.contents).strip('[').strip(']')
+                            datablocks.append(current_block.copy())
+            else:
+                current_block['data']['level'] = int(component.name[1])
+                current_block['data']['text'] = str(component.contents)
                 datablocks.append(current_block.copy())
-                '''
-            case '>':
-                current_block['type'] = 'quote'
-                current_block['data']['text'] = block[1:]
-                current_block['data']['caption'] = ''
-                current_block['data']['alignment'] = 'left'
-                datablocks.append(current_block.copy())
-                '''
-            case '>':
-                if len(block) > 1:
-                    if block[1] and block[2]:
-                        current_block['type'] = 'list'
-                        current_block['data']['style'] = 'unordered'
-                        current_block['data']['items'] = [b.strip('>') for b in block.split('\n')]
-                        datablocks.append(current_block.copy())
-            case '!':
-                current_block['type'] = 'image'
-                current_block['data']['url'] = block[block.find('('):block.find(')')]
-                datablocks.append(current_block.copy())
-            case '<':
-                if re.match('<image>', block):
-                    current_block['type'] = 'image'
-                    current_block['data']['url'] = 'widgets/attachment/' + block.split('>')[-1].split(' ')[0].strip('"').strip("'")
-                    datablocks.append(current_block.copy())
-            case _:
-                current_block['type'] = 'paragraph'
-                current_block['data']['text'] = block.strip(' ')
-                if len(current_block['data']['text'].strip(' ')) > 0:
-                    datablocks.append(current_block.copy())
-    return json.dumps({'blocks': datablocks})
-
-def html_to_js(html_string):
-    converted = html2text.html2text(html_string)
-    processed = text_to_js(converted)
-    return processed
-
-    '''
-    if html_string is '' or html_string is None:
-        return ''
-    datablocks = []
-    html_json = BeautifulSoup(html_string, 'lxml')
-    for component in html_json.find_all(True):
-        print('-----' + str(component.name) + '.......')
-        current_block = {'data': {}}
-        if not re.match(component.name, 'h[1-6]'):
-            match component.name:
-                case 'p':
-                    current_block['type'] = 'paragraph'
-                    current_block['data']['text'] = str(component.contents)
-                    datablocks.append(current_block.copy())
-                case 'delimeter':
-                    current_block['type'] = 'delimeter'
-                    current_block['data'] = {}
-                    datablocks.append(current_block.copy())
-                case 'code':
-                    current_block['type'] = 'code'
-                    current_block['data']['code'] = str(component.contents).strip('`')
-                    datablocks.append(current_block.copy())
-                case 'blockquote':
-                    current_block['type'] = 'quote'
-                    current_block['data']['caption'] = str(component.find('cite').contents[0])
-                    current_block['data']['text'] = str(component.find())
-                case 'ul':
-                    current_block['type'] = 'list'
-                    current_block['data']['style'] = 'unordered'
-                    current_block['data']['items'] = []
-                    for list_entry in component.find_all('li'):
-                        current_block['data']['items'].append(list_entry.contents)
-                    datablocks.append(current_block.copy())
-                case 'ol':
-                    current_block['type'] = 'list'
-                    current_block['data']['style'] = 'ordered'
-                    current_block['data']['items'] = []
-                    for list_entry in component.find_all('li'):
-                        current_block['data']['items'].append(list_entry.contents)
-                    datablocks.append(current_block.copy())
-                case 'form':
-                    current_block['type'] = 'checklist'
-                    current_block['data']['items'] = []
-                    for input in component.find_all('input'):
-                        if input.find('checked') == 'true':
-                            checked: 'True'
-                        else:
-                            checked: 'False'
-                        checkbox = {
-                            'checked': checked,
-                            'text': str(input.find())
-                        }
-                        current_block['data']['items'].append(checkbox)
-                    datablocks.append(current_block.copy())
-                case 'table':
-                    current_block['type'] = 'table'
-                    current_block['data'] = []
-                    current_block['data']['content'] = []
-                    for t_row in component.contents:
-                        if t_row.name == 'th':
-                            current_block['data']['withHeadings'] = True
-                        if t_row.name == 'tr' or t_row.name == 'th':
-                            row = []
-                            for cell in t_row.contents:
-                                if cell.name == 'td':
-                                    row.append(cell.contents[0])
-                            current_block['data']['content'].append(row)
-                    datablocks.append(current_block.copy())
-                case 'img':
-                    current_block['type'] = 'image'
-                    current_block['data']['file'] = {}
-                    current_block['data']['file']['url'] = 'widgets/attachment/' + attachment_id_from_name(component[0].split('/')[-1])
-                case _:
-                    current_block['type'] = 'paragraph'
-                    current_block['data']['text'] = str(component.contents)
-        else:
-            current_block['data']['level'] = int(component.name[1])
-            current_block['data']['text'] = str(component.contents)
-            datablocks.append(current_block.copy())
-            datablocks.append(current_block.copy())
-    '''
-    return text_to_js(html2markdown.convert(html_string))
+        return json.dumps({'blocks': datablocks})
 
 def url_from_tryton_to_flask(url, prefix):
     if not 'widgets/attachment' in url:
