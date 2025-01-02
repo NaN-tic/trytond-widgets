b04b646cff7ae8a49c56075917e8367b0c9d81a3
Albert Cervera i Areny <albert@nan-tic.com>
2025-01-02 17:05:20 +0100

    Allow a 'language' attribute to form fields.

--- ir.py
+++ ir.py
@@ -9,6 +9,7 @@ class View(metaclass=PoolMeta):
     def get_rng(cls, type_):
         rng = super().get_rng(type_)
         if type_ in ('form', 'list-form'):
+            # Add 'block' and 'code' as possible values for the 'widget' attribute
             widgets = rng.xpath(
                 '//ns:define/ns:optional/ns:attribute'
                 '/ns:name[.="widget"]/following-sibling::ns:choice',
@@ -19,4 +20,26 @@ class View(metaclass=PoolMeta):
             subelem = etree.SubElement(widgets,
                 '{http://relaxng.org/ns/structure/1.0}value')
             subelem.text = 'code'
+
+            # Using RNG format allow defining a new attribute named 'language':
+            #
+            # <define name="attlist.field" combine="interleave">
+            #   <optional>
+            #     <attribute>
+            #       <name ns="">language</name>
+            #       <text/>
+            #     </attribute>
+            #   </optional>
+            # </define>
+            define = rng.xpath(
+                '/ns:grammar',
+                namespaces={'ns': 'http://relaxng.org/ns/structure/1.0'})[0]
+            define = etree.SubElement(define, '{http://relaxng.org/ns/structure/1.0}define')
+            define.set('name', 'attlist.field')
+            define.set('combine', 'interleave')
+            optional = etree.SubElement(define, '{http://relaxng.org/ns/structure/1.0}optional')
+            attribute = etree.SubElement(optional, '{http://relaxng.org/ns/structure/1.0}attribute')
+            name = etree.SubElement(attribute, '{http://relaxng.org/ns/structure/1.0}name')
+            name.text = 'language'
+            etree.SubElement(attribute, '{http://relaxng.org/ns/structure/1.0}text')
         return rng
