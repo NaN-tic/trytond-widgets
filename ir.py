6e029902e3ea4352c3c11d4599375719c9aab25e
Raimon Esteve <raimonesteve@gmail.com>
2025-11-25 15:35:40 +0100

    FIX check_xml with rng attributes lxml.etree.RelaxNGParseError: Attributes conflicts in interleave

--- ir.py
+++ ir.py
@@ -9,6 +9,9 @@ class View(metaclass=PoolMeta):
     def get_rng(cls, type_):
         rng = super().get_rng(type_)
         if type_ in ('form', 'list-form'):
+            ns = {'ns': 'http://relaxng.org/ns/structure/1.0'}
+            Q = '{http://relaxng.org/ns/structure/1.0}'
+
             # Add 'block' and 'code' as possible values for the 'widget' attribute
             widgets = rng.xpath(
                 '//ns:define/ns:optional/ns:attribute'
@@ -31,15 +34,33 @@ class View(metaclass=PoolMeta):
             #     </attribute>
             #   </optional>
             # </define>
-            define = rng.xpath(
-                '/ns:grammar',
-                namespaces={'ns': 'http://relaxng.org/ns/structure/1.0'})[0]
-            define = etree.SubElement(define, '{http://relaxng.org/ns/structure/1.0}define')
-            define.set('name', 'attlist.field')
-            define.set('combine', 'interleave')
-            optional = etree.SubElement(define, '{http://relaxng.org/ns/structure/1.0}optional')
-            attribute = etree.SubElement(optional, '{http://relaxng.org/ns/structure/1.0}attribute')
-            name = etree.SubElement(attribute, '{http://relaxng.org/ns/structure/1.0}name')
-            name.text = 'language'
-            etree.SubElement(attribute, '{http://relaxng.org/ns/structure/1.0}text')
+
+            # Find the define used by Tryton for field attributes
+            # IMPORTANT: it may appear twice, but the one we want is
+            # the one with combine="interleave"
+            defines = rng.xpath(
+                '//ns:define[@name="attlist.field" and @combine="interleave"]',
+                namespaces=ns
+            )
+
+            if defines:
+                define = defines[0]
+            else:
+                grammar = rng.xpath('/ns:grammar', namespaces=ns)[0]
+                define = etree.SubElement(grammar, Q + 'define')
+                define.set('name', 'attlist.field')
+                define.set('combine', 'interleave')
+
+            # Check if the 'language' attribute already exists
+            exists = define.xpath(
+                './/ns:attribute/ns:name[.="language"]',
+                namespaces=ns
+            )
+            if not exists:
+                optional = etree.SubElement(define, Q + 'optional')
+                attribute = etree.SubElement(optional, Q + 'attribute')
+                name = etree.SubElement(attribute, Q + 'name')
+                name.text = 'language'
+                etree.SubElement(attribute, Q + 'text')
+
         return rng
